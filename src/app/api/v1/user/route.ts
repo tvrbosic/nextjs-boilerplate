70f298c409b232bbb91ba9cb2de5f8307317d5bb
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2025-02-01 13:18:16 +0100

    WIP

--- src/app/api/v1/user/route.ts
+++ src/app/api/v1/user/route.ts
@@ -1,7 +1,14 @@
+// LIBRARY
 import { NextResponse } from 'next/server';
+
+// APP
+import { prisma } from '@/db/prisma';
 
 export async function GET(request) {
   // Do whatever you want
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
   return NextResponse.json({ message: 'Hello World' }, { status: 200 });
 }
 
