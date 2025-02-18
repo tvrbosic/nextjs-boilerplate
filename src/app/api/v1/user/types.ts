165d931d6a8f8a3d4b720c8eb926f44f4a63a7cc
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2025-02-18 21:28:41 +0100

    User CRUD
    * WIP
    
    * WIP

--- src/app/api/v1/user/types.ts
+++ src/app/api/v1/user/types.ts
@@ -1,3 +1,13 @@
-export interface IApiUserDeleteParams {
-  guid: string;
+import { IHasGuid } from '@/types/global';
+
+export interface IGetUserParams {
+  params: Promise<IHasGuid>;
+}
+
+export interface IPutUserParams {
+  params: Promise<IHasGuid>;
+}
+
+export interface IDeleteUserParams {
+  params: Promise<IHasGuid>;
 }
---
+++ src/types/global.ts
@@ -0,0 +1,3 @@
+export interface IHasGuid {
+  guid: string;
+}
