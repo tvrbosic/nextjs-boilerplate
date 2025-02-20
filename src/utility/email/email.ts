2ee145e573beee11f85ee14bc4af619ce9fe1dff
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2025-02-20 16:02:37 +0100

    WIP

---
+++ src/utility/email/email.ts
@@ -0,0 +1,31 @@
+// LIBRARY
+import nodemailer from 'nodemailer';
+
+// TYPES
+import { ISendEmailProps } from '@/utility/email/types';
+
+const sendEmail = async (options: ISendEmailProps) => {
+  // 1) Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.APP_EMAIL_HOST,
+    port: process.env.APP_EMAIL_PORT,
+    auth: {
+      user: process.env.APP_EMAIL,
+      pass: process.env.APP_EMAIL_PASSWORD,
+    },
+  });
+
+  // 2) Define the email options
+  const mailOptions = {
+    from: 'Tomislav Vrbošić <tomislav.vrbosic@gmail.com>',
+    to: options.destinationEmail,
+    subject: options.subject,
+    text: options.htmlContent,
+    // html: TODO
+  };
+
+  // 3) Send the email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
