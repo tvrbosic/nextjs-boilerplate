f2a62c626ad8431cc4885391e6ffba9b3007125f
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2025-03-13 11:57:57 +0100

    WIP

--- src/components/toggle-switch/toggle-switch.tsx
+++ src/components/toggle-switch/toggle-switch.tsx
@@ -1,4 +1,8 @@
+// TYPES
 import { ToggleSwitchProps } from '@/components/toggle-switch/types';
+
+// STYLES
+import styles from '@/components/toggle-switch/styles.module.css';
 
 export default function ToggleSwitch({
   checked,
@@ -6,20 +10,17 @@ export default function ToggleSwitch({
   onChange,
 }: ToggleSwitchProps) {
   return (
-    <label className="inline-flex items-center cursor-pointer">
+    <label className={styles.toggle_label_container}>
       <input
         type="checkbox"
-        className="sr-only peer"
+        className={styles.toggle_input_hidden}
         checked={checked}
         onChange={onChange}
       />
-      <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-teal-200/40 dark:peer-focus:ring-teal-500/40 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600 dark:peer-checked:bg-teal-600"></div>
 
-      {label && (
-        <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-          {label}
-        </span>
-      )}
+      <span className={styles.toggle_span_track} />
+
+      {label && <span className={styles.toggle_span_text}>{label}</span>}
     </label>
   );
 }
